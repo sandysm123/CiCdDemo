a3a476ebb8ce86ea3a9a75e7ef6562dbba625fb7
sandeepmgupta22 <sandeepmgupta22@gmail.com>
2026-07-25 23:28:59 +0530

    Add CI/CD pipeline: GitHub Actions + ECS Fargate deploy"

---
+++ CiCdDemo.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using CiCdDemo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CiCdDemo.Tests
+{
+    public class HomeControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+    }
+}
